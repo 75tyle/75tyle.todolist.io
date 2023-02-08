958a0ca2bbd68af3f655501e98ef884a774ef6e6
sanghpal <kamblesanghpal2003@gmail.com>
2023-02-08 18:16:05 +0530

    update with new feature

--- list.js
+++ list.js
@@ -1,5 +1,6 @@
-
+$('.updt_task').hide()
 var list = []
+var updt 
 $(document).ready(function(){
   
     const urlParams = new URLSearchParams(window.location.search);
@@ -11,10 +12,16 @@ $(document).ready(function(){
     $('.add_task').click(function(event){
         var task = $('.user_text').val()
         if(task.length != 0){
-            list.push(task)
-            console.log(list)
-            list_data()
+          
             $('.user_text').val('')
+            if(task == '//'){
+             alert('new feature')
+            }else{
+                list.push(task)
+                $('.user_text').focus()
+                console.log(list)
+                list_data()
+            }
         }else{
             alert("task cannot be empty!")
         }
@@ -38,15 +45,40 @@ function list_data(){
     for (var i = 0; i < list.length; i++) {
        var j = i+1
             var data = i;
-        html+='<tr><td>'+j+'</td><td>'+list[i]+'</td><td class="d-flex" ><i class="fa-solid fa-pen p-1"></i> <i class="fa-solid fa-eraser p-1" onclick="delet('+i+')"></i></td> </tr>'
+        html+='<tr><td>'+j+'</td><td>'+list[i]+'</td><td class="d-flex" ><i class="fa-solid fa-pen p-1" onclick="edit('+i+')"></i> <i class="fa-solid fa-eraser p-1" onclick="delet('+i+')"></i></td> </tr>'
 
       }
       $('.list_data').html(html)
 
 }
+
+function edit(id){
+    console.log(id)
+    updt = id
+    $('.user_text').val(list[id])
+    $('.add_task').hide()
+    $('.updt_task').show()
+    $('.user_text').focus()
+}
+
 function delet(data){
     
     var removed = list.splice(data,1)
     list_data();
 }
 
+function update(){
+   var updt_list = $('.user_text').val()
+   if(updt_list.length != 0){
+    list[updt] = updt_list
+    $('.user_text').focus()
+    console.log(list)
+    list_data()
+    $('.user_text').val('')
+    $('.add_task').show()
+    $('.updt_task').hide()
+}else{
+    alert("task cannot be empty!")
+}
+
+}
